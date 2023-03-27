5853a470647159a6a512c92b0b35e04ec3e9b0a0
hathupham <111330275+hathupham@users.noreply.github.com>
2023-03-27 23:12:36 +0700

    Update ha.py

--- ha.py
+++ ha.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/hathuphammm")
+@app.route("/")
 def home():
     return "<h1> Hello, World! </h1>"
+if __name__ == '__main__':
+    app.run()
